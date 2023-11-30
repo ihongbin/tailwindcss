d14f2de4ae148da4df05b46fc8ea430888777460
v_binhong <v_binhong@tencent.com>
2023-11-30 20:09:16 +0800

    init project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css"
+// import logo from "./assets/logo.svg"
+
+function App() {
+  return <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+}
+
+export default App
